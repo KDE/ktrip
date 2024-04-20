baf3072c8311072f67cca8850fb6073f6be39d0d
Nicolas Fella <nicolas.fella@gmx.de>
2024-04-20 13:30:27 +0200

    Spell check experiment

---
+++ spell.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: None
+# SPDX-License-Identifier: CC0-1.0
+
+import json
+
+warnings = []
+
+warning = {
+    "type": "issue",
+    "description": "Test",#match.group(5),
+    "categories": ["Bug Risk"], # TODO
+    "location": {
+        "path": "src/main.cpp",
+        "lines": {
+            "begin": 0
+        }
+    },
+    "fingerprint": 1
+}
+
+warnings.append(warning)
+
+f = open("report.json", "w")
+f.write(json.dumps(warnings))
+f.close()
